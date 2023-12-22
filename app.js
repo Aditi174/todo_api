e9e9efcc93a3e8d511bfc1173f1c76e8b2728d13
Aditi174 <070801ap@gmail.com>
2023-12-22 13:20:58 +0530

    files added

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const container = document.getElementById("List")
 
-async function display(){
+const input = document.getElementById("input")
+
+async function display() {
     const result = await fetch('https://dummyjson.com/todos');
     const data = await result.json()
     console.log(data)
 
     const tododata = data.todos
 
-    for(let i of tododata){
+    for (let i of tododata) {
         const listitems = document.createElement("li")
         listitems.classList.add("items")
         listitems.textContent = i.todo
@@ -16,4 +18,30 @@ async function display(){
     }
 
 }
+
+async function createtodo() {
+    const apidata = await fetch('https://dummyjson.com/todos/add', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            todo: input.value,
+            completed: false,
+            userId: 5,
+        })
+    })
+    return await apidata.json()
+}
+
+async function newlist(){
+    const response = await createtodo()
+    const data = response.todo
+
+    const newlist = document.createElement("li")
+    newlist.classList.add("newlist")
+    newlist.textContent = data
+
+    container.insertBefore(newlist, container.firstChild)
+    input.value = ""
+}
+
 display()
