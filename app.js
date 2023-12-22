a9db85e601843dcc206926b6f6f128b526c829c9
Aditi174 <070801ap@gmail.com>
2023-12-22 16:03:41 +0530

    file added

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
-const container = document.getElementById("List")
+const todocontainer = document.getElementById("List")
 
 const input = document.getElementById("input")
 
-async function display() {
-    const result = await fetch('https://dummyjson.com/todos');
-    const data = await result.json()
-    console.log(data)
+async function displaytodo() {
+    const todoapiresult = await fetch('https://dummyjson.com/todos');
+    const data = await todoapiresult.json()
 
     const tododata = data.todos
 
-    for (let i of tododata) {
+    for (let todos of tododata) {
         const listitems = document.createElement("li")
         listitems.classList.add("items")
-        listitems.textContent = i.todo
+        listitems.textContent = todos.todo
 
-        container.appendChild(listitems)
+        todocontainer.appendChild(listitems)
     }
 
 }
 
-async function createtodo() {
-    const apidata = await fetch('https://dummyjson.com/todos/add', {
+async function addNewTodo() {
+    const newapiresponse = await fetch('https://dummyjson.com/todos/add', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
@@ -29,19 +28,23 @@ async function createtodo() {
             userId: 5,
         })
     })
-    return await apidata.json()
+    return await newapiresponse.json()
 }
 
-async function newlist(){
-    const response = await createtodo()
+async function addNewTodoList(){
+    if(input.value=== ""){
+       return null;
+    }
+    const response = await addNewTodo()
     const data = response.todo
-
-    const newlist = document.createElement("li")
-    newlist.classList.add("newlist")
-    newlist.textContent = data
-
-    container.insertBefore(newlist, container.firstChild)
+    
+    const NewTodoListItems = document.createElement("li")
+    NewTodoListItems.classList.add("newlist")
+    NewTodoListItems.textContent = data
+    
+    todocontainer.insertBefore(NewTodoListItems, todocontainer.firstChild)
     input.value = ""
+
 }
 
-display()
+displaytodo()
